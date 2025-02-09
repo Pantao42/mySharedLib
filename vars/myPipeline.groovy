b8ed482b6e6c357fabc76ff2d83fb9b8ecfc70e2
Pantao42 <pantao@kabelmail.de>
2025-02-09 13:11:08 +0100

    MavenSettings konfiguriert

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -20,11 +20,13 @@ def call(body) {
                 }
             }
             stage("Configure") {
+                steps {
                     loadMavenSettings(fileName: "${mvnSettingsFile}")
 //                    // OpenJDK 21 konfigurieren
 //                    tool name: 'OPENJDK21', type: 'jdk'
 //                    env.JAVA_HOME = tool 'OPENJDK21'
 //                    env.PATH = "${env.JAVA_HOME}/bin:${env.PATH}"
+                }
             }
             stage("Build") {
                 steps {
